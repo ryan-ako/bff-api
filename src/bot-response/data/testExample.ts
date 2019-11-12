15b835e0d1b115862b266404423f4f07218a1897
Ryan Yang <ryan@ako.ai>
2019-11-13 11:07:58 +1300

    add test data for response

---
+++ src/bot-response/data/testExample.ts
@@ -0,0 +1,36 @@
+const data = [
+  {
+    responseCode: 'test_101',
+    category: 'Frontdesk Services',
+    'hint-question': 'Hotel reception desk',
+    'detailed-explanation': '-',
+    example:
+      "The hotel's reception desk is open 24 hours, with our friendly team on standby to assist you with anything from arranging tickets to local shows, to providing newspapers and secure luggage storage.",
+  },
+  {
+    responseCode: 'test_102',
+    category: 'Frontdesk Services',
+    'hint-question': 'Hotel reception desk',
+    'detailed-explanation': '-',
+    example:
+      "The hotel's reception desk is open 24 hours, with our friendly team on standby to assist you with anything from arranging tickets to local shows, to providing newspapers and secure luggage storage.",
+  },
+  {
+    responseCode: 'test_103',
+    category: 'Frontdesk Services',
+    'hint-question': 'Hotel reception desk',
+    'detailed-explanation': '-',
+    example:
+      "The hotel's reception desk is open 24 hours, with our friendly team on standby to assist you with anything from arranging tickets to local shows, to providing newspapers and secure luggage storage.",
+  },
+  {
+    responseCode: 'test_104',
+    category: 'Frontdesk Services',
+    'hint-question': 'Hotel reception desk',
+    'detailed-explanation': '-',
+    example:
+      "The hotel's reception desk is open 24 hours, with our friendly team on standby to assist you with anything from arranging tickets to local shows, to providing newspapers and secure luggage storage.",
+  },
+];
+
+export default data;
